880afe9b8df6b0e4e566f1c0a282fde22f08c236
leonardogomes0227-star <leonardogomes0227@gmail.com>
2026-08-05 21:15:52 -0300

    Remove comment about Supabase URL

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://svwflvetptzgxriboyjp.supabase.co'; // <--- A URL real que você copiou ali
+const supabaseUrl = 'https://svwflvetptzgxriboyjp.supabase.co';
 const supabaseKey = 'sb_publishable_Irta09gMznifr9RgaQcFwQ_NV_IHUdH';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
