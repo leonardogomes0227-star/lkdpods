a476abfe25fcdb6267ed67d8768c448b3f92afec
leonardogomes0227-star <leonardogomes0227@gmail.com>
2026-08-06 00:20:55 -0300

    Update store.tsx

--- src/store.tsx
+++ src/store.tsx
@@ -38,12 +38,9 @@ export const useStore = create<StoreState>((set, get) => ({
       const { data, error } = await supabase.from('products').select('*');
       if (!error && data) {
         set({ products: data });
-      } else {
-        set({ products: [] });
       }
     } catch (err) {
       console.error('Erro ao buscar produtos:', err);
-      set({ products: [] });
     }
   },
 
