bcb33073f08c350c636ed0e7c24c43b7baadbc34
leonardogomes0227-star <leonardogomes0227@gmail.com>
2026-08-11 21:37:50 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Silencia os alerts feios do navegador globalmente
+window.alert = function(msg) {
+  console.log("Aviso: ", msg);
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
